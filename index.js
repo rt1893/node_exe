fa153406bcaa405874e51bda98f6ce8b2498a48b
rt1893 <37836318+rt1893@users.noreply.github.com>
2025-02-20 13:03:47 +0000

    some new again 1

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-const http = require('http');
 const { spawn } = require('child_process');
 const path = require('path');
-
-const hostname = '0.0.0.0'; // Use '0.0.0.0' to listen on all available interfaces
-const port = 5001;  // Port number
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World 5001!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
 
 // Define the path to the Linux executable
 const exePath = path.join(__dirname, 'jiotv_go-linux-386');

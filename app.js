678b4c8efdc74ff6e558d0210181e8660cbbab80
rt1893 <37836318+rt1893@users.noreply.github.com>
2025-02-21 17:34:55 +0000

    nm30

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var http = require('https');
-const port = process.env.PORT || 4000;
 
 //create a server object:
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(port); //the server object listens on port 8080
+}).listen(8080); //the server object listens on port 8080

acf4e78dfef2557929f384169ebd18434409b34d
rt1893 <37836318+rt1893@users.noreply.github.com>
2025-02-22 12:14:08 +0000

    nm52

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const listener = function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
 
   // Send the response body as "Hello World"
-  response.end('<h2 style="text-align: center;">Hello World</h2>');
+  response.end('<h2 style="text-align: center;">Hello World it is on port 443</h2>');
 };
 
 // Create the HTTPS server with the credentials
